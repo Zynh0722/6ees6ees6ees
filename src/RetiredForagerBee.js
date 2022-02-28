81167ecd422fb57bed3b24aa71779f2c10001a89
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-28 10:33:16 -0800

    Finish all
    Im sorry I didn't commit 1 by 1 this time, it was just a direct rehash
    of the last one, and I pretty much didn't pause typing once.

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+
+    this.age = 40;
+    this.color = 'grey';
+    this.canFly = false;
+    this.job = 'gamble';
+  }
+
+  gamble(treasure) {
+    super.forage(treasure);
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+}
